0ed2f462ab34730c2176680a29a095c82cff2f27
SSead <34585838+SSead@users.noreply.github.com>
2018-03-31 23:16:30 +0200

    Radi

--- NewsFeed/PythonApplication1.py
+++ NewsFeed/PythonApplication1.py
@@ -1,7 +1,9 @@
-
 from newsapi import NewsApiClient
 
 newsapi = NewsApiClient(api_key='b5a3ea3530de476cb0a17415ff59cd16')
 
-print(newsapi.get_top_headlines(q='tech'))
+s = str(newsapi.get_top_headlines(q='tech'))
 
+while (s.find('title') > 0):
+    print(s[s.find('title') + 9 : s.find('description') - 4])
+    s = s[s.find('description') + 2:]
