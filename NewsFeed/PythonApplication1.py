70e728c7b57da2a695145c2c23e33138d2e99ad0
ajdinahmetovic <31190418+ajdinahmetovic@users.noreply.github.com>
2018-04-01 10:52:03 +0200

    Serial added

--- NewsFeed/PythonApplication1.py
+++ NewsFeed/PythonApplication1.py
@@ -1,9 +1,16 @@
 from newsapi import NewsApiClient
+import serial
+import time
 
+
+#serial = serial.Serial(port = '/dev/rfcomm0', baudrate=38400)
 newsapi = NewsApiClient(api_key='b5a3ea3530de476cb0a17415ff59cd16')
 
-s = str(newsapi.get_top_headlines(q='tech'))
+while True:
+    s = str(newsapi.get_top_headlines(language='en', q = 'tech'))
 
-while (s.find('title') > 0):
-    print(s[s.find('title') + 9 : s.find('description') - 4])
-    s = s[s.find('description') + 2:]
+    while (s.find('title') > 0):
+        print(s[s.find('title') + 9 : s.find('description') - 4])
+        #serial.write(s[s.find('title') + 9 : s.find('description') - 4])
+        s = s[s.find('description') + 2:]
+        time.sleep(20)
